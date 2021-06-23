bbdbcf088f58864db6cb6bcff8aa08e5f56b61ed
小羽 <821760648@qq.com>
2021-06-24 00:28:51 +0800

    ✨ 修改介绍页面

--- src/index/components/app.vue
+++ src/index/components/app.vue
@@ -1,15 +1,16 @@
 <!--
- * @description: 
- * @author: 小羽
- * @Date: 2021-01-12 18:42:06
- * @LastEditTime: 2021-01-14 14:59:42
- * @Copyright: 1.0.0
+ * @Description: 
+ * @Author: 小羽
+ * @LastEditors: 小羽
+ * @Date: 2021-06-13 15:29:37
+ * @LastEditTime: 2021-06-24 00:16:12
 -->
 <template>
-	<div class="index_page">
-		my-plugin插件
-		<div class="index_page_main">
-			<el-link href="/my-plugin.zip" type="primary" target="_blank">下载</el-link>
+	<div class="index-page">
+		<div class="index-page-main">
+			<h1 class="chrome-title">掘金天气插件</h1>
+			<div class="chrome-description">这是一个chrome天气插件，可作用于掘金网站。</div>
+			<el-link href="/weather-plugin.zip" type="primary" target="_blank">下载</el-link>
 		</div>
 	</div>
 </template>
@@ -21,10 +22,21 @@
 </script>
 
 <style lang="less" scoped>
-	.index_page{
-    text-align: center;
-    width: 100%;
-		.popup_page_main{
+	.index-page{
+    display: flex;
+		justify-content: center;
+		&-main{
+			text-align: center;
+			width: 300px;
+			border: solid 1px #eee;
+			padding: 0 30px 30px 30px;
+			.chrome{
+				&-title{}
+				&-description{
+					text-indent: 2em;
+					text-align: left;
+				}
+			}
 		}
 	}
 </style>
