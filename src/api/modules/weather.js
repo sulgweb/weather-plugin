52a118b14a4c679f40deeb2a22bad1f8b5023b94
小羽 <821760648@qq.com>
2021-06-28 07:39:14 +0800

    处理地理坐标获取异常的问题

--- src/api/modules/weather.js
+++ src/api/modules/weather.js
@@ -3,9 +3,9 @@ import axios from "axios";
 
 class Weather {
   // 获取地理坐标
-  async getAreaLocation(){
-    return await axios.get(`https://map.baidu.com/?qt=ipLocation&t=${new Date().getTime()}`).then(res=>{
-      if(!res.data.rgc.result.location){
+  async getAreaLocation() {
+    return await axios.get(`https://map.baidu.com/?qt=ipLocation&t=${new Date().getTime()}`).then(res => {
+      if (!res.data.rgc || !res.data.rgc.result.location) {
         // 如果定位失败则返回广州的地理位置
         return {
           lat: 23.188855624765,
@@ -17,14 +17,14 @@ class Weather {
   }
 
   // 获取id
-  async getAreaId(lat,lng){
-    let params = {"method":"stationinfo",lat,lng}
-    let res = await axios.get(`https://d4.weather.com.cn/geong/v1/api?params=${encodeURI(JSON.stringify(params))}`).then(res=>res.data.data.station.areaid)
+  async getAreaId(lat, lng) {
+    let params = { "method": "stationinfo", lat, lng }
+    let res = await axios.get(`https://d4.weather.com.cn/geong/v1/api?params=${encodeURI(JSON.stringify(params))}`).then(res => res.data.data.station.areaid)
     return res
   }
   // 获取天气信息
   async getWeather(areaId) {
-    console.log("areaId",areaId)
+    console.log("areaId", areaId)
     let res1 = await axios.get(`https://d1.weather.com.cn/wap40/${areaId}.html?_=${new Date().getTime()}`).then(res => {
       return JSON.parse(res.data.split('=')[1])
     })
